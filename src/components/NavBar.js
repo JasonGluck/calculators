38b3ea3add9a48d60d73b81d5ff39616ebfca141
github username <github email address>
2017-01-19 19:58:10 -0500

    added routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router'
 
 export default () => (
   <div id="nav" class="fourteen columns">
 	<ul>
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Credit Card</a></li>
-		<li><a href="#">Mortgage</a></li>
-		<li><a href="#">Student Loan</a></li>
-    <li><a href="#">Car</a></li>
+		<li><Link to="/">Home</Link></li>
+		<li><Link to="/creditcard">Credit Card</Link></li>
+		<li><Link to="/mortgage">Mortgage</Link></li>
+		<li><Link to="/studentloan">Student Loan</Link></li>
+    <li><Link to="/car">Car</Link></li>
 	</ul>
 </div>
 )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Router, Route, browserHistory } from 'react-router'
 import rootReducer from './ducks/index';
 import App from './components/App';
 import './css/skeleton.css';
@@ -11,7 +12,13 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+      <Route path="/creditcard" component={App} />
+      <Route path="/mortgage" component={App} />
+      <Route path="/studentloan" component={App} />
+      <Route path="/car" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
