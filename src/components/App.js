f8621438a124a716d326a86a0ff460c653e0f57e
github username <github email address>
2017-01-19 19:35:53 -0500

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import NavBar from './NavBar';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Financial Calculators</h2>
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+  <div id="nav" class="fourteen columns">
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Credit Card</a></li>
+		<li><a href="#">Mortgage</a></li>
+		<li><a href="#">Student Loan</a></li>
+    <li><a href="#">Car</a></li>
+	</ul>
+</div>
+)
