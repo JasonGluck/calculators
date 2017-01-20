3d26158598a7135243d4b291e6a3fb1c3ca885ad
github username <github email address>
2017-01-19 19:22:59 -0500

    sorted file structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './ducks/index';
+import App from './components/App';
+import './css/index.css';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
